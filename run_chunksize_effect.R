e8e6f4096a4097d6e49c11be03545a5fe51648fe
Jenna Vergeynst <jenna.vergeynst@ugent.be>
2019-08-21 13:09:59 +0200

    change readin of hydros

--- run_chunksize_effect.R
+++ run_chunksize_effect.R
@@ -1,18 +1,19 @@
-#rm(list=ls())
-#graphics.off()
+# File run on the UGent high performance cluster, 
+# on 18 nodes in parallel, with use of job array running
+# to run 10 repetitions of 6 different rbi's (= 60 cases) in separate jobs
+# Total running time needed per job = 32?? + 40?? hours (less than 30 hours needed for jobs with  large bi's of 60/90)
+
 #args = commandArgs(trailingOnly=TRUE)
 ID <- Sys.getenv("PBS_ARRAYID") # for job array running on hpc
-## For R-studio:
-#setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set file directory as working directory
-setwd('/Users/jennavergeynst/OneDrive - UGent/Sharing_code/YAPS_simulation_study')
-## Outside R-studio:
-#setwd(getSrcDirectory()[1])
 ## On hpc:
-#setwd('/data/gent/vo/000/gvo00048/vsc41096/YAPS_simulation_study')
+setwd('/data/gent/vo/000/gvo00048/vsc41096/YAPS_simulation_study')
 
+# rm(list=ls())
+# setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set file directory as working directory
 PATH <- getwd()
 toa_path <- paste0(PATH, '/results/toa_dfs/')
 tele_path <- paste0(PATH, '/results/teleTracks/')
+hydro_path <- paste0(PATH, '/results/hydros/')
 
 library(yaps)
 library("parallel")
@@ -21,10 +22,8 @@ library(dplyr)
 
 source("wrapper_functions.R")
 
-all_toas <- list.files(toa_path)
-hydros <- read.csv(paste(PATH,'/results/hydros.csv',sep = ''), row.names = 1)
+#all_toas <- list.files(toa_path)
 chunk_list <- c(250, 500, 1000, 5000, 10000)
-#chunk_list <- c(250)
 summary <- data.frame(matrix(ncol = 10, nrow = 1)) # nrow = length(all_toas)*length(chunk_list)
 colnames(summary) <- c("rep", "chunk_size", "chunk_nb", "pingType", "mean_bi", "dist", "mean_real", "mean_est", "nb_pos", "run_time")
 
@@ -45,7 +44,12 @@ rbi_min = as.double(metadata["rbi_min",])
 rbi_max = as.double(metadata["rbi_max",])
 
 csvtag = substring(filename, 8)
+last_part <- strsplit(csvtag,'_')[[1]][[3]]
+rep_part <- strsplit(last_part, '.csv')[[1]][[1]]
+rep <- strsplit(rep_part, "rep")[[1]][[2]]
+
 teleTrack = read.csv(paste0(tele_path,"teleTrack_",csvtag))
+hydros <- read.csv(paste(hydro_path,'hydros_',rep,'.csv',sep = ''), row.names = 1)
 
 toa_rev_superdf <- data.frame()
 teleTrack_superdf <- data.frame()
