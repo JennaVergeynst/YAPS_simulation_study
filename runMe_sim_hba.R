24971b10160a84ff4ecaeaedaa4212f93fd1047e
Henrik Baktoft <hba@aqua.dtu.dk>
2019-10-24 10:42:15 +0200

    finalizing and cleaning up...

--- runMe_sim_hba.R
+++ runMe_sim_hba.R
@@ -1,136 +1,74 @@
 rm(list=ls())
 graphics.off()
-setwd('/home/hbak/H/ms/others/2019-04 JennaVergeynst - simulation study/git/YAPS_simulation_study')
-PATH = "../../data"
-# devtools::install_github("baktoft/yaps", force=TRUE)
+# # # Install relevant branch of YAPS from github - One time operation
+# devtools::install_github("baktoft/yaps", force=TRUE, ref="pbi")
+
+# # # Setup paths to your specific setup...
+script_path <- '/home/hbak/H/ms/others/2019-04 JennaVergeynst - simulation study/git/YAPS_simulation_study'
+data_path = "../../data" # this directory must exsist and contain dirs sim_part_1, sim_part_2, sim_part_3, sim_part_4
+setwd(script_path)
 
 library(yaps)
 library(dplyr)
 library(plyr)
 library(data.table)
-#set.seed(42)
+library(ggplot2)
 source("wrapper_functions.R")
 source('funcs_hba.R')
+#set.seed(42)
 
-# # # simul params
-# crw movement
-crw_shape <- 1
-crw_scale <- 0.5
+#############################
+### Simulate track specified in getSimSetup()
+# # # sim_part 0 - test - short version of everything
+# # # sim_part 1 - effect of pMP
+# # # sim_part 2 - effect of shifts
+# # # sim_part 3 - effect of track length
+# # # sim_part 4 - effect of temporal resolution of hydros
+# specify which part of the study to simulate for...
+sim_part <- 0
+# get the sim_setup....
+sim_setup <- getSimSetup(sim_part=sim_part)
 
-# detection params
-pNA <- 0.5
-pMP <- 0.03
-sbi_sd <- 1e-4
+# get list of simulated tracks etc...
+# this can take quite a long time, so save the results for future use
+simul <- getSimul(sim_setup)
+save(list=c('simul'), file=paste0(data_path, "/sim_part", sim_part,"_sim_dat.rObj"))
 
-# track specifics
-# n_tracks <- 30
-# track_lengths <- c(500, 1000, 5000)
-n_tracks <- 2
-track_lengths <- c(1000, 2500, 5000, 10000)
-# track_lengths <- c(1000, 2500)
-
-# transmitter specifics
-mean_bi = c(1.2, 5, 15, 30, 45, 60, 90)
-# mean_bi = c(1.2, 5)
-min_bi  = c(1.1, 1, 10, 20, 30, 40, 60)
-max_bi  = c(1.3, 9, 20, 40, 60, 80, 120)
-ping_types <- c('sbi', 'rbi')
-# ping_types <- c('sbi')
-
-shifts = c(0, 0.5, 1, 2) # shift = nb of array-lengths the track is moved to the right
-# shifts = c(0, 0.5) # shift = nb of array-lengths the track is moved to the right
-# shifts = c(0, 2) # shift = nb of array-lengths the track is moved to the right
-
-
-# get everything simulated
-# return list of simul stuff - e.g. sim_params, sim_dt, toa_list
-simul <- getSimul(mean_bi, min_bi, max_bi, n_tracks, track_lengths, shifts, sbi_sd, pNA, pMP, crw_shape, crw_scale)
-names(simul)
-
-# # save everything needed to run YAPS to avoid redoing everything everytime...
-save(list=c('simul'), file=paste0(PATH, "/sim_dat.rObj"))
-
-
-# # # When all tracks are simulated and saved, we can start YAPS'ing from here...
-rm(list=ls())
-path <- "../../data"
-setwd('/home/hbak/H/ms/others/2019-04 JennaVergeynst - simulation study/git/YAPS_simulation_study')
-load(file=paste0(path, "/sim_dat.rObj"))
-source('funcs_hba.R')
+# When all tracks are simulated and saved, we can start positioning from here...
+load(file=paste0(data_path, "/sim_part", sim_part,"_sim_dat.rObj"))
+# Yaps sometimes fail, so we give it a chance to retry...
 max_retries <- 100
-
-library(data.table)
-library(yaps)
-ls()
-
-# gb <- doRunYapsParallel(sim_dt=simul$sim_dt, toa_list=simul$toa_list, hydros=simul$hydros, max_retries=max_retries)
-
-sim_dt <- simul$sim_dt
-tt_shifted_list <- simul$tt_shifted_list
-
-files <- list.files(paste0(path, "/yaps_out"))
-sim_ids <- as.numeric(tstrsplit(files, '_')[[2]])
-sim_idx <- 1:length(sim_ids)
-
-out <- c()
-for(i in 1:length(files)){
-	print(i)
-	sim_id <- sim_ids[i]
-	load(file=paste0(path, "/yaps_out/", files[sim_idx[i]]))
-	
-	length(tt_shifted_list)
-	track_shift_id <- as.numeric(sim_dt[sim_id, 'track_shift_id'])
-	
-	tt_shift_i <- tt_shifted_list[[track_shift_id]]
-	
-	tru <- 	data.table(true_time=tt_shift_i$time, true_x=tt_shift_i$x, true_y=tt_shift_i$y)
-	est <- data.table(est_x=outTmb$pl$X, est_y=outTmb$pl$Y, est_top=outTmb$pl$top)
-
-	nobs <- apply(inp$datTmb$toa, 2, function(k) sum(!is.na(k)))
-
-	tru[, roll:=true_time]
-	est[, roll:=est_top]
-	setkey(tru, roll)
-	setkey(est, roll)
-	
-	out_i <- tru[est, roll="nearest"]
-	out_i[, est_top:=roll]
-	out_i[, roll:=NULL]
-	out_i[, ping:=1:.N]
-	out_i[, sim_id:=sim_id]
-	out_i[, nobs:=nobs]
-	
-	out <- rbind(out, out_i)
-}
-
-out[, dev:=sqrt((true_x - est_x)^2 + (true_y - est_y)^2)]
-
-out2 <- merge(out, sim_dt, by='sim_id', sort=FALSE)
-out2[is.na(mean_bi), mean_bi:=(min_bi+max_bi)/2]
-summ <- out2[, list('mean_dev' = mean(dev), 'median_dev' = median(dev), 'min_dev' = min(dev), 'max_dev' = max(dev)), by=c('ping_type','shift', 'mean_bi','sim_id','track_id','track_length')]
-ggplot(data=summ, aes(x=mean_bi, y=log(median_dev), col=factor(track_length), fill=factor(track_length))) + geom_point() + geom_smooth(se=FALSE) + facet_grid(ping_type~shift)
-
-ggplot(data=summ, aes(x=mean_bi, y=log(median_dev), col=factor(track_length), fill=factor(track_length))) + geom_point() + geom_smooth(se=FALSE) + facet_grid(ping_type~shift)
-
-	
-hydros_dt <- data.table(dplyr::bind_rows(hydros, .id="track_id"))
-
-library(ggplot2)
-ggplot(data=out2) + geom_point(aes(x=est_top, y=dev, col=factor(shift))) + facet_grid(track_id~ping_type)
-ggplot(data=out2) + geom_point(aes(x=est_top, y=dev, col=factor(shift))) + facet_grid(mean_bi~ping_type)
-ggplot(data=out2) + geom_point(aes(x=est_top, y=dev, col=factor(track_id))) + facet_grid(shift~ping_type)
-
-ggplot(data=out2[track_id == 31]) + geom_path(aes(x=true_x, y=true_y, group=sim_id),col="blue", size=1.2) + 
-	geom_path(aes(x=est_x, y=est_y, col=factor(mean_bi), group=sim_id)) + 
-	geom_point(data=hydros_dt[track_id == 31], aes(x=hx, y=hy), col="green") +
-	facet_grid(ping_type~shift) + coord_fixed(ratio=1)
+gb <- doRunYapsParallel(simul=simul, max_retries=max_retries, sim_part, data_path)
 
 
-ls()
+###############################
+### post-processing and plotting
+# Takes a long time, so save when done for future use...
+summ_part1 <- collate_output(sim_part=0, data_path)
+summ_part1 <- collate_output(sim_part=1, data_path)
+summ_part2 <- collate_output(sim_part=2, data_path)
+summ_part3 <- collate_output(sim_part=3, data_path)
+summ_part4 <- collate_output(sim_part=4, data_path)
 
+save(summ_part1, file=paste0(data_path, "/summ_part1.rObj"))
+save(summ_part2, file=paste0(data_path, "/summ_part2.rObj"))
+save(summ_part3, file=paste0(data_path, "/summ_part3.rObj"))
+save(summ_part4, file=paste0(data_path, "/summ_part4.rObj"))
 
+load(file=paste0(data_path, "/summ_part1.rObj"))
+load(file=paste0(data_path, "/summ_part2.rObj"))
+load(file=paste0(data_path, "/summ_part3.rObj"))
+load(file=paste0(data_path, "/summ_part4.rObj"))
 
+p1 <- ggplot(data=summ_part1, aes(x=jitter(mean_bi), y=log(median_dev), col=factor(ping_type), fill=factor(ping_type))) + geom_point(alpha=0.3) + geom_smooth() +  facet_grid(.~pMP)
+p2 <- ggplot(data=summ_part2, aes(x=jitter(mean_bi), y=log(median_dev), col=factor(ping_type), fill=factor(ping_type))) + geom_point(alpha=0.3) + geom_smooth() +  facet_grid(.~shift)
+p3 <- ggplot(data=summ_part3, aes(x=jitter(mean_bi), y=log(median_dev), col=factor(ping_type), fill=factor(ping_type))) + geom_point(alpha=0.3) + geom_smooth() +  facet_grid(.~track_length)
+p4 <- ggplot(data=summ_part4, aes(x=jitter(mean_bi), y=log(median_dev), col=factor(ping_type), fill=factor(ping_type))) + geom_point(alpha=0.3) + geom_smooth() +  facet_grid(.~htr)
 
+p1 <- p1 + theme_minimal() + theme(legend.position="none", plot.background=element_rect(fill="grey90"), axis.line=element_line(color="black")) + coord_cartesian(ylim=c(-4,4)) + viridis::scale_color_viridis(discrete=TRUE, option="D") + viridis::scale_fill_viridis(discrete = TRUE, option="D") + labs(title="pMP")
+p2 <- p2 + theme_minimal() + theme(legend.position="none", plot.background=element_rect(fill="grey90"), axis.line=element_line(color="black")) + coord_cartesian(ylim=c(-4,4)) + viridis::scale_color_viridis(discrete=TRUE, option="D") + viridis::scale_fill_viridis(discrete = TRUE, option="D") + labs(title="Shifts")
+p3 <- p3 + theme_minimal() + theme(legend.position="none", plot.background=element_rect(fill="grey90"), axis.line=element_line(color="black")) + coord_cartesian(ylim=c(-4,4)) + viridis::scale_color_viridis(discrete=TRUE, option="D") + viridis::scale_fill_viridis(discrete = TRUE, option="D") + labs(title="Track length")
+p4 <- p4 + theme_minimal() + theme(legend.position="none", plot.background=element_rect(fill="grey90"), axis.line=element_line(color="black")) + coord_cartesian(ylim=c(-4,4)) + viridis::scale_color_viridis(discrete=TRUE, option="D") + viridis::scale_fill_viridis(discrete = TRUE, option="D") + labs(title="Hydrophone Temp Resolution")
 
-
+gridExtra::grid.arrange(p1, p2, p3, p4, nrow = 2, ncol=2)
 
